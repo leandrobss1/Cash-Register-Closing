44be8d0e5eacfcf6f79dfd409b067a70f4275e56
leandrobss1 <leandroborgesdev@gmail.com>
2026-06-08 00:43:20 -0300

    chore(react-router-dom): Incluido biblioteca para gerenciar + funções de acesso na página.

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div className="container-home">
+      <h1 className="title-home">O que deseja fazer hoje?</h1>
+      <button>Fechamento de Caixa</button>
+      <button>Sangria de Caixa</button>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
-import { StrictMode } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { createRoot } from "react-dom/client";
-import { GlobalStyle } from "./styles/GlobalStyles.ts";
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
+import Home from "./components/Home/Home.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <GlobalStyle />
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-    <App />
-    <Analytics />
-  </StrictMode>,
+        <Route path="fechamento-caixa" element={<App />}></Route>
+      </Routes>
+      <Analytics />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
